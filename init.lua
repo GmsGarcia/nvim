3a531a710a57405367442e97cdc3c8d123915d3c
GmsGarcia <gmsgarcia18@gmail.com>
2024-05-08 00:22:56 +0100

    added mason

--- init.lua
+++ init.lua
@@ -1,17 +1,20 @@
+require("gmsgarcia.globals")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {}
+require("gmsgarcia.autocmds")
+require("gmsgarcia.keymaps")
+require("gmsgarcia.options")
 
-require("vim-options")
 require("lazy").setup("plugins")
