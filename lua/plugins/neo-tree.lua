594919308eba11e9ec641a3d7aede526a9c33f89
GmsGarcia <gmsgarcia18@gmail.com>
2024-05-14 14:50:04 +0100

    added go lsp

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,18 +1,23 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		vim.keymap.set("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", {})
-		require("neo-tree").setup({
-			filesystem = {
-				hijack_netrw_behavior = "open_current",
-			},
-		})
-	end,
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>ee", ":Neotree filesystem toggle left<CR>", {})
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+        hijack_netrw_behavior = "open_current",
+      },
+    })
+  end,
 }
