79715d0ece2ee73783b7eb8ed08d3359e9a7ff3d
GmsGarcia <gmsgarcia18@gmail.com>
2024-06-07 13:31:40 +0100

    binds fix

--- lua/gmsgarcia/globals.lua
+++ lua/gmsgarcia/globals.lua
@@ -5,14 +5,14 @@ _G.IS_MAC = OS == "Darwin"
 _G.IS_LINUX = OS == "Linux"
 _G.IS_WINDOWS = OS:find("Windows") and true or false
 _G.IS_WSL = (function()
-	local output = vim.fn.systemlist("uname -r")
-	local condition1 = IS_LINUX and uname.release:lower():find("microsoft") and true or false
-	local condition2 = not not string.find(output[1] or "", "WSL")
-	return condition1 or condition2
+  local output = vim.fn.systemlist("uname -r")
+  local condition1 = IS_LINUX and uname.release:lower():find("microsoft") and true or false
+  local condition2 = not not string.find(output[1] or "", "WSL")
+  return condition1 or condition2
 end)()
 
 -- Wrapper of print + vim.inspect
 P = function(v)
-	print(vim.inspect(v))
-	return v
+  print(vim.inspect(v))
+  return v
 end
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,95 +1,95 @@
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  require("completion").on_attach(client)
-  vim.lsp.inlay_hint.enable(bufnr)
-
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-  vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+	require("completion").on_attach(client)
+	vim.lsp.inlay_hint.enable(bufnr)
 end
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "rust_analyzer",
-          "omnisharp",
-          "omnisharp_mono",
-          "intelephense",
-          "html",
-          "cssls",
-          "tsserver",
-          "jsonls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local pid = vim.fn.getpid()
-      local omnisharp_bin = "/usr/local/bin/omnisharp-roslyn/OmniSharp"
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"rust_analyzer",
+					"omnisharp",
+					"omnisharp_mono",
+					"intelephense",
+					"html",
+					"cssls",
+					"tsserver",
+					"jsonls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local pid = vim.fn.getpid()
+			local omnisharp_bin = "/usr/local/bin/omnisharp-roslyn/OmniSharp"
 
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local opt = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      }
+			local opt = {
+				capabilities = capabilities,
+				on_attach = on_attach,
+			}
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup(opt)
-      lspconfig.gopls.setup(opt)
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          ["rust-analyzer"] = {
-            imports = {
-              granularity = {
-                group = "module",
-              },
-              prefix = "self",
-            },
-            cargo = {
-              buildScripts = {
-                enable = true,
-              },
-            },
-            procMacro = {
-              enable = true,
-            },
-          },
-        },
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup(opt)
+			lspconfig.gopls.setup(opt)
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					["rust-analyzer"] = {
+						imports = {
+							granularity = {
+								group = "module",
+							},
+							prefix = "self",
+						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+						},
+						procMacro = {
+							enable = true,
+						},
+					},
+				},
+			})
 
-      -- gamedev
-      --[[lspconfig.omnisharp.setup({
+			-- gamedev
+			--[[lspconfig.omnisharp.setup({
 				cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
 				on_attach = on_attach,
 				capabilities = capabilities,
 			})]]
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      -- webdev
-      lspconfig.intelephense.setup(opt) -- php
-      lspconfig.html.setup(opt)      -- html
-      lspconfig.cssls.setup(opt)     -- css
-      lspconfig.tsserver.setup(opt)  -- js/ts
-      lspconfig.jsonls.setup(opt)    -- json
-    end,
-  },
+			-- webdev
+			lspconfig.intelephense.setup(opt) -- php
+			lspconfig.html.setup(opt) -- html
+			lspconfig.cssls.setup(opt) -- css
+			lspconfig.tsserver.setup(opt) -- js/ts
+			lspconfig.jsonls.setup(opt) -- json
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
