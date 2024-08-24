1f1e425feeee0d7bb1c99bba924958de5dba1570
GmsGarcia <gmsgarcia18@gmail.com>
2024-08-24 13:00:44 +0100

    startup theme + lspstuff

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -10,25 +10,26 @@ return {
         null_ls.builtins.formatting.prettier,
         null_ls.builtins.diagnostics.phpcs,
 
+        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.csharpier,
         --null_ls.builtins.diagnostics.gol angcilsp,
       },
       on_attach = function(client, bufnr)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = bufnr,
-						callback = function()
-							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-							vim.lsp.buf.format({ async = false })
-						end,
-					})
-				end
-			end,
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
 
-    vim.keymap.set("n", "<leader>fc", vim.lsp.buf.format, {})
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
   end,
 }
