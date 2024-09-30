50bf64d91ce69e5a62d6a1ca6a1aa28db54706f0
GmsGarcia <gmsgarcia18@gmail.com>
2024-09-30 10:31:59 +0100

    added kotlin

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,93 +1,95 @@
 local on_attach = function(client, bufnr)
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-  --require("completion").on_attach(client) -- this broke after 0.10 neovim update :/
-  --vim.lsp.inlay_hint.enable(bufnr) -- this too
+	--require("completion").on_attach(client) -- this broke after 0.10 neovim update :/
+	--vim.lsp.inlay_hint.enable(bufnr) -- this too
 end
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "gopls",
-          "rust_analyzer",
-          "clangd",
-          "intelephense",
-          "html",
-          "cssls",
-          "tsserver",
-          "jsonls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"gopls",
+					"rust_analyzer",
+					"clangd",
+					"intelephense",
+					"html",
+					"cssls",
+					"tsserver",
+					"jsonls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local opt = {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      }
+			local opt = {
+				capabilities = capabilities,
+				on_attach = on_attach,
+			}
 
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup(opt)
-      lspconfig.gopls.setup(opt)
-      lspconfig.rust_analyzer.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-        settings = {
-          ["rust-analyzer"] = {
-            imports = {
-              granularity = {
-                group = "module",
-              },
-              prefix = "self",
-            },
-            cargo = {
-              buildScripts = {
-                enable = true,
-              },
-            },
-            procMacro = {
-              enable = true,
-            },
-          },
-        },
-      })
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup(opt)
+			lspconfig.gopls.setup(opt)
+			lspconfig.rust_analyzer.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					["rust-analyzer"] = {
+						imports = {
+							granularity = {
+								group = "module",
+							},
+							prefix = "self",
+						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+						},
+						procMacro = {
+							enable = true,
+						},
+					},
+				},
+			})
 
-      lspconfig.clangd.setup(opt) -- c & cpp
+			lspconfig.clangd.setup(opt) -- c & cpp
 
-      -- gamedev
-      --[[lspconfig.omnisharp.setup({
+			-- gamedev
+			--[[lspconfig.omnisharp.setup({
 				cmd = { omnisharp_bin, "--languageserver", "--hostPID", tostring(pid) },
 				on_attach = on_attach,
 				capabilities = capabilities,
 			})]]
-      lspconfig.csharp_ls.setup({
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
+			lspconfig.csharp_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
 
-      -- webdev
-      lspconfig.intelephense.setup(opt) -- php
-      lspconfig.html.setup(opt)      -- html
-      lspconfig.cssls.setup(opt)     -- css
-      lspconfig.tsserver.setup(opt)  -- js/ts
-      lspconfig.jsonls.setup(opt)    -- json
+			lspconfig.kotlin_language_server.setup({}) -- kotlin
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			-- webdev
+			lspconfig.intelephense.setup(opt) -- php
+			lspconfig.html.setup(opt) -- html
+			lspconfig.cssls.setup(opt) -- css
+			lspconfig.tsserver.setup(opt) -- js/ts
+			lspconfig.jsonls.setup(opt) -- json
+
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,35 +1,37 @@
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.phpcbf,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.phpcs,
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.diagnostics.ktlint,
+				null_ls.builtins.diagnostics.phpcs,
 
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.csharpier,
-        --null_ls.builtins.diagnostics.gol angcilsp,
-      },
-      on_attach = function(client, bufnr)
+				null_ls.builtins.formatting.ktfmt,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.phpcbf,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.csharpier,
+				--null_ls.builtins.diagnostics.gol angcilsp,
+			},
+			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
-          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-              -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-              vim.lsp.buf.format({ async = false })
-            end,
-          })
-        end
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+							-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end
 			end,
-    })
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
